e52e2e70dae097fda9cff7c93cc0ad52ddddcb75
nooberfsh <nooberfsh@gmail.com>
2020-07-12 12:10:58 +0800

    add alias

--- config/config.fish
+++ config/config.fish
@@ -2,3 +2,6 @@ set -x HDX "$HOME/hdx"
 set -x CARGO_HOME "$HDX/apps/cargo"
 set -x RUSTUP_HOME "$HDX/apps/rustup"
 set -x PATH "$HDX/apps/cargo/bin:$HDX/bin:$PATH"
+
+alias pts="phoronix-test-suite"
+alias ee="nvim $HOME/.config/fish/config.fish"
