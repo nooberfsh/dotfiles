4ed5e9bfe3d7046e88ee1c47e7e6c83e2062c4db
nooberfsh <nooberfsh@gmail.com>
2020-12-11 13:03:39 +0800

    fish: update system default editor

--- config/config.fish
+++ config/config.fish
@@ -7,5 +7,8 @@ set -x GOPATH "$HDX/apps/go"
 
 set -x PATH "$HDX/apps/go/bin:$HDX/apps/cargo/bin:$HDX/bin:$PATH"
 
+set -x VISUAL "nvim"
+set -x EDITOR "$VISUAL"
+
 alias pts="phoronix-test-suite"
 alias ee="nvim $HOME/.config/fish/config.fish"
