1bb4a3b078046cda09cc9323091d1a027ea53638
nooberfsh <nooberfsh@gmail.com>
2020-12-11 09:52:45 +0800

    fish: add GOPATH

--- config/config.fish
+++ config/config.fish
@@ -1,7 +1,11 @@
+set fish_greeting
+
 set -x HDX "$HOME/hdx"
 set -x CARGO_HOME "$HDX/apps/cargo"
 set -x RUSTUP_HOME "$HDX/apps/rustup"
-set -x PATH "$HDX/apps/cargo/bin:$HDX/bin:$PATH"
+set -x GOPATH "$HDX/apps/go"
+
+set -x PATH "$HDX/apps/go/bin:$HDX/apps/cargo/bin:$HDX/bin:$PATH"
 
 alias pts="phoronix-test-suite"
 alias ee="nvim $HOME/.config/fish/config.fish"
